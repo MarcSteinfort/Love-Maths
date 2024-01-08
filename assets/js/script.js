eba7b21af5d0e558b5604fb5bc816583510a5f59
Marc Steinfort <msteinfort@web.de>
2024-01-08 16:43:04 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function runGamincrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubstractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivideQuestion(){
+
+}
